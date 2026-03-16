1d89d37c66421d71d8145a4f6c9641b306b1f74d
ASWIN <aswinkrishna798@gmail.com>
2026-03-16 13:42:47 +0530

    Fix backend sightengine credentials in config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 
-SIGHTENGINE_API_USER = os.getenv("502965771")
-SIGHTENGINE_API_SECRET = os.getenv("QBtwpVMA7yFKP6etxhzuzUwCzru8dztA")
+SIGHTENGINE_API_USER = os.environ.get("SIGHTENGINE_API_USER", "502965771")
+SIGHTENGINE_API_SECRET = os.environ.get("SIGHTENGINE_API_SECRET", "QBtwpVMA7yFKP6etxhzuzUwCzru8dztA")
 
 SIGHTENGINE_ENDPOINT = "https://api.sightengine.com/1.0/check.json"
